71075ab5754ca0895afeaa3d8734e05ba614e483
SHAKIB_SOFT <Shakib2077>
2024-04-06 14:34:49 +0600

    templete done

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -95,7 +95,7 @@
       <div class="footer_section layout_padding">
          <div class="container">
             <div class="input_btn_main">
-               <input type="text" class="mail_text" placeholder="Enter your email" name="Enter your email">
+               <input type="text" class="mail_text" placeholder="Enter your email" name="email">
                <div class="subscribe_bt"><a href="#">Subscribe</a></div>
             </div>
             <div class="location_main">
